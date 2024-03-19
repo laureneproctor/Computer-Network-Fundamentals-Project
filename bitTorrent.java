d339a2ca1d9f7084ceebe839a6fb82c9e4e21638
Sofia Celestrin <sofiacelestrin@ufl.edu>
2024-03-19 16:55:53 -0400

    Comments added to bitTorrent

--- bitTorrent.java
+++ bitTorrent.java
@@ -1,48 +1,101 @@
+// Packages needed
 import java.io.*;
 import java.nio.*;
 
-class Handshake {
+// Class for the handshake
+class Handshake 
+{
+	// Handshake header that is a 18-byte string
 	private final String header = "P2PFILESHARINGPROJ";
+
+	// 10-byte zero bits
 	private final byte[] zeros = new byte[10];
+
+	// Integer of peer ID
 	private int peerId;
 
-	public Handshake(int peerId) {
+	// Constructor that defines the peerID
+	public Handshake(int peerId) 
+	{
 		this.peerId = peerId;
 	}
 
-	public byte[] getBytes() {
+	// Function that creates and returns a byte array which is the handshake message
+	public byte[] getBytes() 
+	{
+		// Creatingthe ByteArrayOutputSteam to write bytes into
 		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-		try {
+		
+		// Try and catch to write the bytes
+		try 
+		{
+			// Writing the header
 			byteArrayOutputStream.write(header.getBytes());
+
+			// Writing the zeros
 			byteArrayOutputStream.write(zeros);
+
+			// Writing the peerID
 			byteArrayOutputStream.write(ByteBuffer.allocate(4).putInt(peerId).array());
-		} catch (IOException e) {
+		} 
+		
+		// This is used to handle any errors
+		catch (IOException e) 
+		{
 			e.printStackTrace();
 		}
+
+		// Returns the output stream as a byte array
 		return byteArrayOutputStream.toByteArray();
 	}
 }
 
-class Message {
+// Class for the message
+class Message
+ {
+	// The length field of the messafe that is 4-bytes
 	private int length;
+
+	// 1-byte message type field
 	private byte type;
+	
+	// Message payload that can be different sizes
 	private byte[] payload;
 
-	public Message(byte type, byte[] payload) {
+	// Constructor used to define the variables type, payload, and length
+	public Message(byte type, byte[] payload) 
+	{
 		this.type = type;
 		this.payload = payload;
 		this.length = payload.length;
 	}
 
-	public byte[] getBytes() {
+	// Function that creates and returns a byte array which is the message
+	public byte[] getBytes() 
+	{
+		// Creates an object to write the bytes into
 		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-		try {
+		
+		// Using try and catch to write bytes into the output stream and catch errors
+		try 
+		{
+			// Writing into the length
 			byteArrayOutputStream.write(ByteBuffer.allocate(4).putInt(length).array());
+			
+			// Writing into the type
 			byteArrayOutputStream.write(type);
+
+			// Writing into the payload
 			byteArrayOutputStream.write(payload);
-		} catch (IOException e) {
+		}
+
+		// Catching errors and printing out the error
+		 catch (IOException e) 
+		 {
 			e.printStackTrace();
 		}
+
+		// Returns byte array created
 		return byteArrayOutputStream.toByteArray();
 	}
 }
